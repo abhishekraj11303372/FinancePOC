7c53708ec83c4be8f4be39315729c27aeead62c5
abhishekraj11303372 <43966764+abhishekraj11303372@users.noreply.github.com>
2020-08-16 01:47:28 +0530

    routing done

--- FinancePOC/FinancePOC.Web/FinancePOC-Web/src/app/app-routing.module.ts
+++ FinancePOC/FinancePOC.Web/FinancePOC-Web/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewerComponent } from './viewer/viewer.component';
+import { FinanaceDetailsComponent } from './finanace-details/finanace-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'viewer', component: ViewerComponent },
+  { path: 'finance', component: FinanaceDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

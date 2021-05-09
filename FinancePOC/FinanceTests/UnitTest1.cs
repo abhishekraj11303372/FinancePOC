1da3123ed3495c1032c165dd58adee9725ecbfe8
abhishekraj11303372 <43966764+abhishekraj11303372@users.noreply.github.com>
2021-05-09 10:42:20 +0530

    new changes lock removed

---
+++ FinancePOC/FinanceTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace FinanceTests
+{
+    public class FinanceConversionTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+        [Fact]
+        public void UnableToConvert()
+        {
+            //Arrange
+            //var convert = new Convert();
+            //Act
+
+            //Assert
+
+        }
+    }
+}
